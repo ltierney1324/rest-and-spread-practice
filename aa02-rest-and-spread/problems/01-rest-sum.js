0ea977a1182647047f4626222ab86dec6453f0f9
Your Name <Your Email>
2024-05-09 19:33:04 -0400

    working pr1

--- aa02-rest-and-spread/problems/01-rest-sum.js
+++ aa02-rest-and-spread/problems/01-rest-sum.js
@@ -5,11 +5,16 @@ them.
 **Hint**: Use rest parameter syntax!
 */
 
-// Your code here 
+// Your code here
+const restSum = (...num) =>{
+  let sum =0;
+  sum +=num;
+  return sum;
+}
 
-// console.log(restSum(3,5,6));                     // => 14
-// console.log(restSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
-// console.log(restSum(0));                         // => 0
+console.log(restSum(3,5,6));                     // => 14
+console.log(restSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(restSum(0));                         // => 0
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/

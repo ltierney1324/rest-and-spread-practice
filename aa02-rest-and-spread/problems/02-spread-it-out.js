9a4fbe0b0229013fe9ed8eddcda3fe7fc5896c22
Garrelchung <garrelchung@gmail.com>
2024-05-09 17:12:04 -0700

    submit

--- aa02-rest-and-spread/problems/02-spread-it-out.js
+++ aa02-rest-and-spread/problems/02-spread-it-out.js
@@ -4,10 +4,12 @@ uses spread operator syntax to return a single array.
 */
 
 // Your code here 
+const spreadItOut = ( arr1, arr2) => [...arr1, ...arr2];
 
-// console.log(spreadItOut([3,5,6], [1,2,3])); // => [3,5,6,1,2,3];
-// console.log(spreadItOut([], [1,2,3]));      // => [1,2,3];
-// console.log(spreadItOut(["apple", "banana"], [1,2,3])); // => ["apple", "banana", 1, 2, 3];
+
+console.log(spreadItOut([3,5,6], [1,2,3])); // => [3,5,6,1,2,3];
+console.log(spreadItOut([], [1,2,3]));      // => [1,2,3];
+console.log(spreadItOut(["apple", "banana"], [1,2,3])); // => ["apple", "banana", 1, 2, 3];
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
